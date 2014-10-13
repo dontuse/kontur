07e46459530856e8769ba81ae02a571ce8fb208d
Andrey Bayandin <dontusecascade@gmail.com>
2014-10-13 21:40:43 +0600

    commit

---
+++ static/js/KonturComponent.js
@@ -0,0 +1,58 @@
+function KonturComponent(data) {
+
+    var self = this;
+
+    self.data = {
+        elements: []
+    };
+    self.data.elements = ko.observableArray([
+        {
+            val: ko.observable(12313),
+            edit: ko.observable(false)
+        }
+    ]);
+
+
+    self.render = function () {
+    };
+
+    self.makeSumm = function () {
+    };
+
+    self.addItem = function () {
+        console.log(this);
+        this.data.elements.unshift({val: ko.observable(), edit: ko.observable(true)});
+    };
+
+    self.removeItem = function (elem) {
+        var that = this;
+        self.data.elements.remove(elem);
+    };
+
+
+    self.saveItem = function (elem) {
+        elem.edit(false);
+        elem.val(+elem.val());
+        console.log(elem.val().constructor.name);
+        if(!(elem.val().constructor.name === 'Number') || isNaN(elem.val()) ) {
+            elem.val(0);
+        }
+    };
+
+    self.startEdit = function (elem) {
+        console.log("edit start");
+        elem.edit(true);
+    };
+
+
+    self.total = ko.computed(function () {
+        var summ = 0;
+        var val = 0;
+        for (var i = 0; self.data.elements().length > i; i++) {
+
+            summ += +self.data.elements()[i].val();
+        }
+
+        return summ;
+    });
+}
