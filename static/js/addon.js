9da65378bc4a56f2820a8d072d579e538c159463
Andrey Bayandin <dontusecascade@gmail.com>
2014-10-13 23:33:04 +0600

    cucu

--- static/js/addon.js
+++ static/js/addon.js
@@ -2,11 +2,9 @@ ko.bindingHandlers.currency = {
     symbol: ko.observable('$'),
     update: function(element, valueAccessor, allBindingsAccessor){
         return ko.bindingHandlers.text.update(element,function(){
-            var value = +(ko.utils.unwrapObservable(valueAccessor()) || 0),
-                symbol = ko.utils.unwrapObservable(allBindingsAccessor().symbol === undefined
-                    ? allBindingsAccessor().symbol
-                    : ko.bindingHandlers.currency.symbol);
-            return  value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+            var value = +(ko.utils.unwrapObservable(valueAccessor()) || 0)
+
+            return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
         });
     }
 };
